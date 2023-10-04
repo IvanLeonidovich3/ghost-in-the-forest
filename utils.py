40b7a603465d787c958b72128714e68afc0e6e8d
Ivan Zuev <ivan.zuev.1984@mail.ru>
2023-10-04 20:55:14 +0300

    requirement

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import pygame
 font_name = pygame.font.match_font('arial')
 
 
-
 def draw_text(surf, text, size, x, y):
     """написание заданного текста на экране игры"""
     font = pygame.font.Font(font_name, size)
@@ -11,5 +10,3 @@ def draw_text(surf, text, size, x, y):
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
-
-
